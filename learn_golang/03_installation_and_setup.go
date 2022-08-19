35bc671c50cc6ca9c1750b76a8127c59363307f6
Lemuel <alvorithmemail@gmail.com>
2022-08-19 12:35:59 +0200

    installation completed with homebrew or standard website method

--- learn_golang/03_installation_and_setup.go
+++ learn_golang/03_installation_and_setup.go
@@ -3,3 +3,5 @@
 // installation instructions to be followed on the website:
 // https://go.dev/doc/install
 // follow the instructions pertiaing to the your OS of your machine
+// can use homebrew to install golang. It also comes with automatic upgrade
+// comamndss for go. Homebrew is a modern packagae manager for gmac iand linux
