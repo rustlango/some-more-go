2c91fa8518972d5a69a6a8f9b410c3a81b8935c0
Lemuel <alvorithmemail@gmail.com>
2022-08-19 11:33:51 +0200

    already have golang installed - latest verison installed

---
+++ learn_golang/03_installation_and_setup.go
@@ -0,0 +1,5 @@
+// Installation instructions
+
+// installation instructions to be followed on the website:
+// https://go.dev/doc/install
+// follow the instructions pertiaing to the your OS of your machine
